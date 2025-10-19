dc93468f775e99248a50c422b543e37ee93a7dcf
Anniken Strand <annikeqs@stud.ntnu.no>
2025-10-19 12:06:35 +1000

    Add base UNet architecture

---
+++ recognition/Improved_UNet_Anniken_Strand_s4979993/modules.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+
+class DoubleConv(nn.Module):
+    # Basic block with two 3x3 conv layers + ReLU, used throughout the network
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        out = self.conv(x)
+        return out
+class UNet(nn.Module):
+    # Basic U-Net implementation for image segmentation
+    # Has an encoder (down) and decoder (up) part with skip connections between them
+    def __init__(self, input_channels=1, output_channels=4, base=64):
+        super(UNet, self).__init__()
+        # encoder
+        self.enc1 = DoubleConv(input_channels, 64)
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.enc2 = DoubleConv(64, 128)
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.enc3 = DoubleConv(128, 256)
+        self.pool3 = nn.MaxPool2d(2)
+
+        # bottleneck
+        self.bottleneck = DoubleConv(256, 512)
+
+        # decoder
+        self.up3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.dec3 = DoubleConv(512, 256)
+
+        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec2 = DoubleConv(256, 128)
+
+        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec1 = DoubleConv(128, 64)
+
+        # output layer
+        self.out = nn.Conv2d(64, output_channels, kernel_size=1)
+
+    def forward(self, x):
+        # encoder
+        e1 = self.enc1(x)
+        e2 = self.enc2(self.pool1(e1))
+        e3 = self.enc3(self.pool2(e2))
+        b = self.bottleneck(self.pool3(e3))
+
+        # decoder (skip connections)
+        d3 = self.dec3(torch.cat([self.up3(b), e3], dim=1))
+        d2 = self.dec2(torch.cat([self.up2(d3), e2], dim=1))
+        d1 = self.dec1(torch.cat([self.up1(d2), e1], dim=1))
+
+        return self.out(d1)
