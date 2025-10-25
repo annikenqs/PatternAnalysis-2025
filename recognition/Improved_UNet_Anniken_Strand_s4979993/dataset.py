2ae3928e8a8de89e5bb4ac41413095b002137b98
Anniken Strand <annikeqs@stud.ntnu.no>
2025-10-25 13:13:47 +1000

    Load HipMRI dataset

--- recognition/Improved_UNet_Anniken_Strand_s4979993/dataset.py
+++ recognition/Improved_UNet_Anniken_Strand_s4979993/dataset.py
@@ -1,20 +1,18 @@
 import os
-from PIL import Image
 import torch
 from torch.utils.data import Dataset
 from torchvision import transforms
+import nibabel as nib
 import numpy as np
+import torch.nn.functional as F
 
-class OASISSegmentationDataset(Dataset):
-    def __init__(self, image_dir, mask_dir, transform=None):
+class HipMRISegmentationDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None, target_size=(256, 256)):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.mask_files = sorted(os.listdir(self.mask_dir))
-        self.transform = transform or transforms.Compose([
-            transforms.ToTensor(),
-            # normalise around 0
-            transforms.Normalize(mean=[0.5], std=[0.5])
-        ])
+        self.transform = transform or transforms.Normalize(mean=[0.5], std=[0.5])
+        self.target_size = target_size
 
     def __len__(self):
         return len(self.mask_files)
@@ -24,19 +22,26 @@ class OASISSegmentationDataset(Dataset):
         # get matching image and mask names
         mask_name = self.mask_files[idx]
         img_name = mask_name.replace("seg_", "case_")
+        
+        # load image and mask
+        img = nib.load(os.path.join(self.image_dir, img_name)).get_fdata()
+        mask = nib.load(os.path.join(self.mask_dir, mask_name)).get_fdata()
 
-        img_path = os.path.join(self.image_dir, img_name)
-        mask_path = os.path.join(self.mask_dir, mask_name)
+        # convert to tensor
+        img = torch.tensor(np.squeeze(img), dtype=torch.float32).unsqueeze(0)  
+        mask = torch.tensor(np.squeeze(mask).astype(np.int64))               
 
-        img = Image.open(img_path)
-        mask = Image.open(mask_path)
-        img = self.transform(img)
-        mask_np = np.array(mask, dtype=np.uint8)
+        # resize image to 256x256 
+        img = img.unsqueeze(0)                          
+        img = F.interpolate(img, size=self.target_size, mode="bilinear", align_corners=False)
+        img = img.squeeze(0)                             
 
-        # map pixel values to class IDs
-        mapping = {0: 0, 85: 1, 170: 2, 255: 3}
-        mask_np = np.vectorize(mapping.get)(mask_np)
+        # resize mask to 256x256 
+        mask = mask.unsqueeze(0).unsqueeze(0).float()        
+        mask = F.interpolate(mask, size=self.target_size, mode="nearest")
+        mask = mask.squeeze().long()
 
-        mask = torch.from_numpy(mask_np).long()
+        # normalize
+        img = (img - img.mean())/(img.std() + 1e-8)
 
         return img, mask
