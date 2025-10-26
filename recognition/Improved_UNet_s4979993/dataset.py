3a875fb3e8d80bee32a6ea4708f9145649bbd0fd
Anniken Strand <annikeqs@stud.ntnu.no>
2025-10-26 11:04:29 +1000

    Adjust resizing

--- recognition/Improved_UNet_s4979993/dataset.py
+++ recognition/Improved_UNet_s4979993/dataset.py
@@ -1,17 +1,14 @@
 import os
 import torch
 from torch.utils.data import Dataset
-from torchvision import transforms
 import nibabel as nib
 import numpy as np
 import torch.nn.functional as F
-
 class HipMRISegmentationDataset(Dataset):
-    def __init__(self, image_dir, mask_dir, transform=None, target_size=(256, 256)):
+    def __init__(self, image_dir, mask_dir, target_size=(256, 128)):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.mask_files = sorted(os.listdir(self.mask_dir))
-        self.transform = transform or transforms.Normalize(mean=[0.5], std=[0.5])
         self.target_size = target_size
 
     def __len__(self):
@@ -28,20 +25,20 @@ class HipMRISegmentationDataset(Dataset):
         mask = nib.load(os.path.join(self.mask_dir, mask_name)).get_fdata()
 
         # convert to tensor
-        img = torch.tensor(np.squeeze(img), dtype=torch.float32).unsqueeze(0)  
-        mask = torch.tensor(np.squeeze(mask).astype(np.int64))               
+        img = torch.tensor(img, dtype=torch.float32).unsqueeze(0) 
+        mask = torch.tensor(mask, dtype=torch.int64)               
 
-        # resize image to 256x256 
+        # resize image to 256x128
         img = img.unsqueeze(0)                          
         img = F.interpolate(img, size=self.target_size, mode="bilinear", align_corners=False)
         img = img.squeeze(0)                             
 
-        # resize mask to 256x256 
+        # resize mask to 256x128
         mask = mask.unsqueeze(0).unsqueeze(0).float()        
         mask = F.interpolate(mask, size=self.target_size, mode="nearest")
         mask = mask.squeeze().long()
 
-        # normalize
+        # normalise
         img = (img - img.mean())/(img.std() + 1e-8)
 
         return img, mask
