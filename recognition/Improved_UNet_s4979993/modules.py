4a932ddec9d417cbdf04c1e0adf79dfbe981f35d
Anniken Strand <annikeqs@stud.ntnu.no>
2025-10-29 10:20:37 +1000

    Refactor architecture with deep supervision and stride downsamples

--- recognition/Improved_UNet_s4979993/modules.py
+++ recognition/Improved_UNet_s4979993/modules.py
@@ -1,9 +1,11 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
 
 class ResidualDoubleConv(nn.Module):
     """
-    Basic block with two 3x3 conv layers, InstanceNorm, and LeakyReLU. Used throughout the network
+    Basic block with two 3x3 conv layers, InstanceNorm and LeakyReLU. Used throughout the network
     """
     def __init__(self, in_channels, out_channels):
         super().__init__()
@@ -11,6 +13,7 @@ class ResidualDoubleConv(nn.Module):
             nn.InstanceNorm2d(in_channels),
             nn.LeakyReLU(0.01, inplace=True),
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.Dropout2d(0.3),
             nn.InstanceNorm2d(out_channels),
             nn.LeakyReLU(0.01, inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False)
@@ -32,49 +35,66 @@ class ImprovedUNet(nn.Module):
     includes encoder-decoder structure with skip connections, 
     using InstanceNorm, LeakyReLU, and pre-activation residual blocks
     """
-    def __init__(self, input_channels=1, output_channels=6, base=64):
+    def __init__(self, input_channels=1, output_channels=6):
         super(ImprovedUNet, self).__init__()
+
         # encoder
-        self.enc1 = ResidualDoubleConv(input_channels, 64)
-        self.pool1 = nn.MaxPool2d(2)
+        self.enc1 = ResidualDoubleConv(input_channels, 16)
+        self.down1 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
 
-        self.enc2 = ResidualDoubleConv(64, 128)
-        self.pool2 = nn.MaxPool2d(2)
+        self.enc2 = ResidualDoubleConv(32, 32)
+        self.down2 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
 
-        self.enc3 = ResidualDoubleConv(128, 256)
-        self.pool3 = nn.MaxPool2d(2)
+        self.enc3 = ResidualDoubleConv(64, 64)
+        self.down3 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
+
+        self.enc4 = ResidualDoubleConv(128, 128)
+        self.down4 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
 
         # bottleneck
-        self.bottleneck = ResidualDoubleConv(256, 512)
-        self.dropout = nn.Dropout2d(0.3)
+        self.bottleneck = ResidualDoubleConv(256, 256)
 
         # decoder
-        self.up3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
-        self.dec3 = ResidualDoubleConv(512, 256)
+        self.up4 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec4 = ResidualDoubleConv(256, 128)
 
-        self.up2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.dec2 = ResidualDoubleConv(256, 128)
+        self.up3 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec3 = ResidualDoubleConv(128, 64)
 
-        self.up1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.dec1 = ResidualDoubleConv(128, 64)
+        self.up2 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
+        self.dec2 = ResidualDoubleConv(64, 32)
 
-        # output layer
-        self.out = nn.Conv2d(64, output_channels, kernel_size=1)
+        self.up1 = nn.ConvTranspose2d(32, 16, kernel_size=2, stride=2)
+        self.dec1 = ResidualDoubleConv(32, 16)
+
+        # outputs
+        self.seg3 = nn.Conv2d(64, output_channels, kernel_size=1)
+        self.seg2 = nn.Conv2d(32, output_channels, kernel_size=1)
+        self.seg1 = nn.Conv2d(16, output_channels, kernel_size=1)
+
 
     def forward(self, x):
         # encoder
         e1 = self.enc1(x)
-        e2 = self.enc2(self.pool1(e1))
-        e3 = self.enc3(self.pool2(e2))
-        b = self.dropout(self.bottleneck(self.pool3(e3)))
+        e2 = self.enc2(self.down1(e1))
+        e3 = self.enc3(self.down2(e2))
+        e4 = self.enc4(self.down3(e3))
+        b = self.bottleneck(self.down4(e4))
 
-
-        # decoder (skip connections)
-        d3 = self.dec3(torch.cat([self.up3(b), e3], dim=1))
+        # decoder with skip connections
+        d4 = self.dec4(torch.cat([self.up4(b), e4], dim=1))
+        d3 = self.dec3(torch.cat([self.up3(d4), e3], dim=1))
         d2 = self.dec2(torch.cat([self.up2(d3), e2], dim=1))
         d1 = self.dec1(torch.cat([self.up1(d2), e1], dim=1))
 
-        return self.out(d1)
+        # deep supervision outputs
+        seg3 = F.interpolate(self.seg3(d3), size=d1.shape[2:], mode="bilinear", align_corners=False)
+        seg2 = F.interpolate(self.seg2(d2), size=d1.shape[2:], mode="bilinear", align_corners=False)
+        seg1 = self.seg1(d1)
+
+        # sum up the predictions
+        out = seg1 + seg2 + seg3
+        return out
 
 # Dice loss function
 def dice_loss(pred, target, eps=1e-6):
